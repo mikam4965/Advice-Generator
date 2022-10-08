f631c497321a5bf784e15f8ec3bce85329546632
mikam4965 <mikam4965@gmail.com>
2022-10-08 15:35:08 +0600

    Create React App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import './style.css';
+import React, { Component } from 'react';
+import MyImage from "./img/icon-dice.svg"
+import logo from "./img/pattern-divider-desktop.svg";
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    advice: '',
+    id: ''
+  };
+
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+
+  fetchAdvice = () => {
+    axios.get('https://api.adviceslip.com/advice')
+      .then((response) => {
+        const { advice } = response.data.slip;
+        const { id } = response.data.slip;
+        this.setState({ advice });
+        this.setState({ id });
+      })
+
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
+  render() {
+    const { advice } = this.state;
+    const { id } = this.state;
+    return (
+      <div className="hero">
+        <div className="container">
+          <div className="number">
+            <p>Advice #{this.state.id}</p>
+          </div>
+          <div className="quote">
+            <h2>"{this.state.advice}"</h2>
+          </div>
+          <div className="logo">
+            <img src={logo} />
+          </div>
+          <button onClick={this.fetchAdvice}>
+            <img src={MyImage} />
+          </button>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
